6c31b99f6711f57025087482d78d302d3e7917a9
Gillian Martel <gillianshome@googlemail.com>
2020-02-29 07:33:01 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading;
+
+namespace CoreConsoleGillian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            while (i < 10)
+            {
+                Thread.Sleep(2000);
+                Console.WriteLine(GetLetter());
+                i++;
+            }
+        }
+
+        private static char GetLetter()
+        {
+            const string chars = "1234567890qwertyuiopasdfghjklzxcvbnm!\"£$%^&*()";
+            var rand = new Random();
+            var num = rand.Next(0, chars.Length - 1);
+            return chars[num];
+        }
+    }
+}
